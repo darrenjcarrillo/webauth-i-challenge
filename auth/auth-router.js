9d2cc77c71ccbf71f562323906ff5adcb473a4d0
Darren J Carrillo <darrenjcarrillo@gmail.com>
2019-11-12 13:00:46 -0800

    Update code

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -5,19 +5,32 @@ const router = require("express").Router();
 const Users = require("../users/users-model.js");
 
 router.post("/register", (req, res) => {
-  let userInformation = req.body;
+  // let userInformation = req.body;
 
-  bcrypt.hash(userInformation.password, 12, (err, hashedPasswod) => {
-    userInformation.password = hashedPasswod;
+  // bcrypt.hash(userInformation.password, 12, (err, hashedPassword) => {
+  //   userInformation.password = hashedPassword;
 
-    Users.add(userInformation)
-      .then(saved => {
-        res.status(201).json(saved);
-      })
-      .catch(error => {
-        res.status(500).json(error);
-      });
-  });
+  //   Users.add(userInformation)
+  //     .then(saved => {
+  //       req.session.username = saved.username; // <<<<<<<<<<<<<<
+  //       res.status(201).json(saved);
+  //     })
+  //     .catch(error => {
+  //       res.status(500).json(error);
+  //     });
+  // });
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      req.session.username = saved.username; // <<<<<<<<<<<<<<
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 router.post("/login", (req, res) => {
@@ -28,7 +41,7 @@ router.post("/login", (req, res) => {
     .then(user => {
       // check that the password is valid
       if (user && bcrypt.compareSync(password, user.password)) {
-        req.session.username = user.username;
+        req.session.user = user;
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: "'You shall not pass!'" });
@@ -53,7 +66,7 @@ router.get("/logout", (req, res) => {
       }
     });
   } else {
-    res.status(200).json({ message: "You were never here to begin with " });
+    res.status(200).json({ message: "You were never here to begin with" });
   }
 });
 
--- users/users-model.js
+++ users/users-model.js
@@ -15,13 +15,18 @@ function findBy(filter) {
   return db("users").where(filter);
 }
 
-function add(user) {
-  return db("users")
-    .insert(user, "id")
-    .then(ids => {
-      const [id] = ids;
-      return findById(id);
-    });
+// function add(user) {
+//   return db("users")
+//     .insert(user, "id")
+//     .then(ids => {
+//       const [id] = ids;
+//       return findById(id);
+//     });
+// }
+async function add(user) {
+  const [id] = await db("users").insert(user);
+
+  return findById(id);
 }
 
 function findById(id) {
